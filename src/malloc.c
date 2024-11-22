db98d54b40d4a2d9a3c24a84b05605479a6e955b
Konrad Kreczko <konradkreczko@gmail.com>
2024-11-22 23:36:26 +0100

    feat: Added union header_t in malloc

--- src/malloc.c
+++ src/malloc.c
@@ -2,6 +2,19 @@
 #include <unistd.h>
 #include "malloc.h"
 
+typedef char ALIGN[16];
+
+union header {
+    struct {
+        size_t size;
+        unsigned is_free;
+        union header *next;
+    } s;
+    ALIGN stub;
+};
+
+typedef union header header_t;
+
 void *malloc(size_t size) {
     void *block;
     block = sbrk(size);
