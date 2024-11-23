278401632f30f6727b1212232132035aedd5a6f7
Konrad Kreczko <konradkreczko@gmail.com>
2024-11-23 17:43:59 +0100

    feat: Something

--- src/malloc.c
+++ src/malloc.c
@@ -22,7 +22,7 @@ pthread_mutex_t global_malloc_lock;
 void *malloc(size_t size) {
     void *block;
     block = sbrk(size);
-    if (block == (void*) - 1) {
+    if (block == (void*) -1) {
         return NULL;
     }
     return block;
