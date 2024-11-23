9413d26d6fd38bed7e1464ec27ae310e8f81fb9d
Konrad Kreczko <konradkreczko@gmail.com>
2024-11-23 17:59:51 +0100

    feat: Rewrite haha :smile:

--- src/malloc.c
+++ src/malloc.c
@@ -1,29 +1,5 @@
-#include <aio.h>
-#include <bits/pthreadtypes.h>
 #include <unistd.h>
 #include "malloc.h"
 
-typedef char ALIGN[16];
+void *global_base = NULL; // pierwszy wskaznik
 
-union header {
-    struct {
-        size_t size;
-        unsigned is_free;
-        union header *next;
-    } s;
-    ALIGN stub;
-};
-
-typedef union header header_t;
-
-header_t *head, *tail;
-pthread_mutex_t global_malloc_lock;
-
-void *malloc(size_t size) {
-    void *block;
-    block = sbrk(size);
-    if (block == (void*) -1) {
-        return NULL;
-    }
-    return block;
-}
--- src/malloc.h
+++ src/malloc.h
@@ -1,1 +1,11 @@
+#include <unistd.h>
+
+typedef struct header {
+    size_t size;
+    struct header *next;
+    int free;
+} header;
+
+#define HEADER_SIZE sizeof(header)
+
 void *malloc(size_t size);
