70b0ab975cde84d8ec614918b33201cfce2578be
Konrad Kreczko <konradkreczko@gmail.com>
2024-11-23 19:29:37 +0100

    feat: Created malloc and free

--- src/malloc.c
+++ src/malloc.c
@@ -29,3 +29,41 @@ header *request_space(header *last, size_t size) {
     block->free = 0;
     return block;
 }
+
+void *malloc(size_t size) {
+    header *block;
+
+    if (size <= 0) {
+        return NULL;
+    }
+    
+    if (!global_base) {
+        block = request_space(NULL, size);
+        if (!block) {
+            return NULL;
+        }
+
+        global_base = block;
+    } else {
+        header *last = global_base;
+        block = find_free_block(&last, size);
+        if (!block) {
+            block = request_space(last, size);
+            if (!block) {
+                return NULL;
+            }
+        } else {
+            block->free = 0;
+        }
+    }
+    return (block + 1);
+}
+
+void free(void *ptr) {
+    if (!ptr) {
+        return;
+    }
+
+    header *block_ptr = (header* )ptr - 1;
+    block_ptr->free = 1;
+}
--- src/malloc.h
+++ src/malloc.h
@@ -9,4 +9,5 @@ typedef struct header {
 #define HEADER_SIZE sizeof(header)
 
 header *find_free_block(header **last, size_t size);
+header *request_space(header *last, size_t size);
 void *malloc(size_t size);
